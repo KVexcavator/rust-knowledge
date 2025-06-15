b8dfee710f258f079f6199f17af602f4d44d2533
Excavator <kvronin@gmail.com>
2025-06-15 14:17:27 +0500

    add exercises

---
+++ 100/exercises/01_intro/00_welcome/src/lib.rs
@@ -0,0 +1,15 @@
+
+// cargo test
+fn greeting() -> &'static str {
+    "I'm ready to learn Rust!"
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::greeting;
+
+    #[test]
+    fn test_welcome() {
+        assert_eq!(greeting(), "I'm ready to learn Rust!");
+    }
+}
